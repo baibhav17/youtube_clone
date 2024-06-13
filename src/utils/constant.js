4825f84c2eb8292bd0e252dff31076d771d2fa0c
baibhav17 <baibhavsxn.035@gmail.com>
2024-06-13 09:54:30 +0530

    removed api key

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-// export const YOUTUBE_API_KEY = 'AIzaSyBTUwdT7mWApS8qgfg6ZfsQ0EiTGYeGmeg';
+export const YOUTUBE_API_KEY = 'YOUR_API_KEY';
 export const YOUTUBE_API_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+YOUTUBE_API_KEY;
 
 export const YOUTUBE_AOTUCOMPLETE_API_URL = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
